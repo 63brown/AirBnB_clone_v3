5b9703e27a872e8f51861a204c3c5ebc0a188b94
63brown <mutheemumbi01@gmail.com>
2023-05-08 02:01:36 +0300

    changes

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+$(document).ready(function () {
+    const amenities = {};
+  
+    $('input[type="checkbox"]').change(function () {
+      if ($(this).is(':checked')) {
+        amenities[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amenities[$(this).data('id')];
+      }
+  
+      const amenitiesList = Object.values(amenities).join(', ');
+      $('.amenities h4').text(amenitiesList);
+    });
+  });
+  
